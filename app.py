a56d5aa6d547d5eba57d1e9ba5f9faa60705163b
Gökdeniz Köksal <kog1wa1@bosch.com>
2026-02-19 16:09:13 +0100

    Mounting app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import requests
 import os
+from langchain_community.llms import Ollama
+#from qdrant_client import QdrantClient
+#from langchain_community.embeddings import FastEmbedEmbeddings
 
-st.title("Ollama Remote Interface")
+st.title("CatGPT says Hello!")
 
 OLLAMA_URL = os.getenv("OLLAMA_HOST", "http://ollama:11434")
+#embeddings = FastEmbedEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 
+#vectorstore = QdrantVectorStore(client=client, collection_name="Berichtsheft", embeddings=embeddings)
+#retriever = vectorstore.as_retriever()
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -13,7 +19,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Schreib Ollama etwas..."):
+if prompt := st.chat_input("Waiting for prompt..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
